f361421113988f266c9969111a26ec028bd80187
Jose Luis <wildchamo@gmail.com>
2025-02-09 13:33:57 -0500

    update env

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const env = process.env.NODE_ENV;
 const config = {
   env: env || "dev",
-  isProd: process.env.NODE_ENV === "production",
   port: process.env.PORT || 3000,
   dbUrl: env === "test" ? process.env.MONGO_URL_TEST : process.env.MONGO_URL,
   dbName: process.env.MONGO_DB_NAME,
